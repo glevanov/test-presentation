5450086937e6a8653bb88d5d272dd2ce0595fc30
Grigoriy Levanov <glevanov@gmail.com>
2018-11-05 12:14:52 +0300

    Animates slider movement, except slider snap

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import '../sass/main.scss';
 import '../img/arrow-down.png';
 
-const SLIDER_DEFAULT_POSITION = 0;
 const SLIDER_PIN_WIDTH = 42.5;
 const SLIDER_PIN_MIDPOINT = SLIDER_PIN_WIDTH / 2;
 
@@ -11,17 +10,44 @@ const sliderFill = sliderElement.querySelector('.slider__fill');
 const sliderPin = sliderElement.querySelector('.slider__pin');
 
 const onSliderTouch = (evt) => {
-  // Надо пересмотреть лекцию Игоря
-  // По клику на слайдер - отключить обработчик скролла, потом вернуть
-
-  const sliderXPosition = evt.touches[0].clientX;
+  let sliderXPosition = evt.touches[0].clientX;
 
   const SLIDER_LEFT_COORDINATE = sliderScale.getBoundingClientRect().left;
   const SLIDER_RIGHT_COORDINATE = sliderScale.getBoundingClientRect().right;
   const SLIDER_WIDTH = SLIDER_RIGHT_COORDINATE - SLIDER_LEFT_COORDINATE;
 
-  const onTouchMove = (moveEvt) => {
+  let sliderRelativePosition;
 
+  const checkSliderBoundaries = (sliderPosition) => {
+    if (sliderPosition < 0 - SLIDER_PIN_MIDPOINT) {
+      return 0 - SLIDER_PIN_MIDPOINT;
+    } else if (sliderPosition > SLIDER_WIDTH - SLIDER_PIN_MIDPOINT) {
+      return SLIDER_WIDTH - SLIDER_PIN_MIDPOINT;
+    }
+    return sliderPosition;
+  };
+
+  const renderSliderPosition = () => {
+    sliderPin.style.left = `${sliderRelativePosition}px`;
+    sliderFill.style.width = `${sliderRelativePosition + SLIDER_PIN_MIDPOINT}px`;
+  };
+
+  const snapSliderPosition = () => {
+    // Прикрутить к onTouchUp
+  };
+
+  const onTouchMove = (moveEvt) => {
+    moveEvt.preventDefault();
+
+    const calculateSliderPosition = () => {
+      const currentTouchX = moveEvt.touches[0].clientX;
+      const horizontalShift = sliderXPosition - currentTouchX;
+      sliderRelativePosition = checkSliderBoundaries(sliderPin.offsetLeft - horizontalShift);
+      sliderXPosition = currentTouchX;
+    };
+
+    calculateSliderPosition();
+    renderSliderPosition();
   };
 
   const onTouchUp = (upEvt) => {
